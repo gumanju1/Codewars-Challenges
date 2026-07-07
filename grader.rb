1b62d48c24609e5212fb6f4aca1905c7acdf1f70
sweta <sweeta.gumanju@gmail.com>
2026-07-07 10:34:44 +0545

    grade marks

---
+++ grader.rb
@@ -0,0 +1,19 @@
+score = 94
+
+if score >= 90
+  puts "A+"
+elsif score >= 80
+  puts "A"
+elsif score >= 70
+  puts "B+"
+elsif score >= 60
+  puts "B"
+elsif score >= 50
+  puts "C+"
+elsif score >= 40
+  puts "C"
+elsif score >= 30
+  puts "D+"
+elsif score >= 20
+  puts "F"
+end
