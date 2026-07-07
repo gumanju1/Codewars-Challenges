7323058b3047d642c83156a5023cdec71a6411e4
sweta <sweeta.gumanju@gmail.com>
2026-07-07 16:35:31 +0545

    rock paper scissors

--- get_the_middle_character.rb
+++ get_the_middle_character.rb
@@ -30,21 +30,3 @@ end
 #Test.assert_equals(get_middle("testing"), "t")
 #Test.assert_equals(get_middle("middle"), "dd")
 #Test.assert_equals(get_middle("A"), "A")
-
-
-#describe("Tests") do
-
-  def do_test(input, expected)
-    actual = get_middle(input)
-    message = 'expected "' + expected + '" but got "' + actual + '" for string "' + input + '"'
-    expect(actual).to eq(expected), message
-  end
-
-  it("Sample Tests") do
-    do_test("test","es")
-    do_test("testing","t")
-    do_test("middle","dd")
-    do_test("A","A")
-    do_test("of","of")
-  end
-end
--- tip_calculator.rb
+++ tip_calculator.rb
@@ -19,3 +19,29 @@ puts calculate_tip(30, "Excellent")
 puts calculate_tip(20, "Good")
 puts calculate_tip(107.65, "Great")
 puts calculate_tip(20, "Bad")
+
+
+
+
+
+
+
+
+
+#another method
+def calculate_tip(amount, rating)
+  case rating.downcase
+  when "terrible"
+    0
+  when "poor"
+    (amount * 0.05).ceil
+  when "good"
+    (amount * 0.10).ceil
+  when "great"
+    (amount * 0.15).ceil
+  when "excellent"
+    (amount * 0.20).ceil
+  else
+    "Rating not recognised"
+  end
+end
