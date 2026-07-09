5e61a4d258e55a46704cb98303910436ae6f79ff
sweta <sweeta.gumanju@gmail.com>
2026-07-09 12:06:52 +0545

    reversed words

--- reversedwords.rb
+++ reversedwords.rb
@@ -5,6 +5,18 @@ end
 
 
 
+puts solution("hello sweta")
+# sweta hello
+
+puts solution("The greatest victory is that which requires no battle")
+# battle no requires which that is victory greatest The
+
+puts solution("apple")
+# apple
+
+puts solution("Ruby is fun")
+# fun is Ruby
+
 
 
 
