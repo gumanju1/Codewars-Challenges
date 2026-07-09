c8e4a910a4f2412e7b0d7085acaad9ca7da9fa71
sweta <sweeta.gumanju@gmail.com>
2026-07-09 11:35:33 +0545

    statistics algorithm calculate mean

--- statistics_algorithm_calculate_mean.rb
+++ statistics_algorithm_calculate_mean.rb
@@ -4,3 +4,14 @@ def calc_mean(ary)
 
   ary.sum.to_f / ary.length
 end
+
+
+
+
+
+# Testing 
+puts "Testing with 'tacos': #{calc_mean('tacos')}"
+puts "Testing with []: #{calc_mean([])}"
+puts "Testing with [1, 2, 3]: #{calc_mean([1, 2, 3])}"
+puts "Testing with [6, 8, 10]: #{calc_mean([6, 8, 10])}"
+puts "Testing with [15, 30, 60, 120, 240]: #{calc_mean([15, 30, 60, 120, 240])}"
